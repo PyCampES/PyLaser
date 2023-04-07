9622e3877209581047ac86237d4037feafb122af
Manuel Kaufmann <humitos@gmail.com>
2023-04-07 18:45:16 +0200

    Send hit

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import time
+import sys
+import machine
+
+ENERGY = 100
+PLAYER = 'nachillo'
+PATH = '/hit/'
+PORT = 8000
+HOST = 'http://192.68.5.182:{PORT}{PATH}'
+MAC = 'a1b2c3'
+
 def do_connect():
     import network
     wlan = network.WLAN(network.STA_IF)
@@ -10,11 +21,35 @@ def do_connect():
     print('network config:', wlan.ifconfig())
 
 
+def send_hit(energy):
+    import socket
+    _, _, host, path = url.split('/', 3)
+    addr = socket.getaddrinfo(host, port)[0][-1]
+    s = socket.socket()
+    s.connect(addr)
+    s.send(bytes(f'GET {PATH}?player={PLAYER}&mac={MAC}&energy={ENERGY} HTTP/1.0\r\nHost: {HOST}\r\n\r\n', 'utf8'))
+    while True:
+        data = s.recv(100)
+        if data:
+            print(str(data, 'utf8'), end='')
+        else:
+            break
+    s.close()
 
-import time
-import machine
-# do_connect()
+do_connect()
+
+
 pin = machine.Pin(34, machine.Pin.IN)
+print(f"Energy: {ENERGY}")
 while True:
-    print("Lectura del laser: ", pin.value())
-    time.sleep(0.5)
+    hit = not pin.value()
+    if hit:
+        ENERGY -= 1
+        print('HIT!')
+        print(f"Energy: {ENERGY}")
+        send_hit(ENERGY)
+
+    if ENERGY <= 0:
+        print('Die!!!')
+        sys.exit()
+    time.sleep(0.1)
